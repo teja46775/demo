8bf490a7603f0d6e994c2f195ad4695c77fb8032
Ubuntu <ubuntu@ip-172-31-28-231.ec2.internal>
2025-02-24 11:30:35 +0000

    intial

---
+++ src/test/java/teja/AppTest.java
@@ -0,0 +1,19 @@
+package teja;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
